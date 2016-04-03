efc8f839504915c250a61928a3b625811dd02132
Christian <buoc_12@hotmail.com>
2016-04-02 20:00:09 -0600

    peliculas listas

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Movies
 
-# Register your models here.
+
+class MoviesAdmin(admin.ModelAdmin):
+    list_display = ['titulo', 'cine', 'horario']
+
+admin.site.register(Movies, MoviesAdmin)
--- movies/models.py
+++ movies/models.py
@@ -3,16 +3,15 @@ from django.db import models
 
 class Movies(models.Model):
     CINE_CHOICES = {
-        ('cmxr', 'Cinemex Reforma')
-        ('cmxc', 'Cinemex Coapa')
-    }
+        ('cmxr', 'Cinemex Reforma'),
+        ('cmxc', 'Cinemex Coapa')}
 
     HORARIOS_CHOICES = {
-        ('hr', ('13:00', '14:00', '20:00'))
-        ('hc', ('15:00', '17:00', '22:00'))
-    }
+        ('hr', '20:00'),
+        ('hc', '15:00')}
 
     titulo = models.CharField(max_length=100)
     cine = models.CharField(max_length=50, choices=CINE_CHOICES)
     horario = models.CharField(max_length=50, choices=HORARIOS_CHOICES)
-    imagen = models.ImageField('upload_to=')
+    imagen = models.ImageField(upload_to='media')
+    
