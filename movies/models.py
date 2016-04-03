3ed2fe480133e3721539aafa0fcbc8853ad3f61e
Christian <buoc_12@hotmail.com>
2016-04-02 19:32:31 -0600

    cambios

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Movies(models.Model):
+    CINE_CHOICES = {
+        ('cmxr', 'Cinemex Reforma')
+        ('cmxc', 'Cinemex Coapa')
+    }
+
+    HORARIOS_CHOICES = {
+        ('hr', ('13:00', '14:00', '20:00'))
+        ('hc', ('15:00', '17:00', '22:00'))
+    }
+
+    titulo = models.CharField(max_length=100)
+    cine = models.CharField(max_length=50, choices=CINE_CHOICES)
+    horario = models.CharField(max_length=50, choices=HORARIOS_CHOICES)
+    imagen = models.ImageField('upload_to=')
