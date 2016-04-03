026d20fdd9e410e6214e3d2a26f942f6994935eb
mafer.564 <madelin_fernanda@hotmail.com>
2016-04-02 19:40:05 -0600

    login

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import View
 
-# Create your views here.
+from .forms import LoginForm
+# from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+
+
+
+class LoginView(View):
+	def get(self,request):
+		form=LoginForm()
+		template_name="accounts/login.html"
+		context={
+		'form':form
+		}
+		return render(request,template_name,context)
+
+	def post(self,request):
+		form=LoginForm(request.POST)
+		if form.is_valid():
+			cd=form.cleaned_data
+			user=authenticate(username=cd['username'],
+				password=cd['password'])
+			if user is not None:
+				login(request,user)
+				return redirect('home')
+			else:
+				return render(request,"accounts/login.html",{'form':form})
